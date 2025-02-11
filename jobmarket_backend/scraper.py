dc5429c5676c9158ca5317af3b396b7237f7da57
alfredoperry14 <alfredoperry14@gmail.com>
2025-02-10 18:27:46 -0800

    importing chrome driver manager

--- jobmarket_backend/scraper.py
+++ jobmarket_backend/scraper.py
@@ -5,6 +5,7 @@ import re
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC

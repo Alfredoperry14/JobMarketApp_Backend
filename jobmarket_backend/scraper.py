4bb539cb7e30357734f8c5fc8e2a165ffbd52b1e
alfredoperry14 <alfredoperry14@gmail.com>
2025-02-10 17:25:18 -0800

    Headless mode

--- jobmarket_backend/scraper.py
+++ jobmarket_backend/scraper.py
@@ -47,6 +47,8 @@ def scrape_jobs():
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                                 "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--user-data-dir=/tmp/chrome_unique")
     
     driver = webdriver.Chrome(options=chrome_options)
     db = SessionLocal()
